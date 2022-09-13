39e04e9d2b5e545557b6a61096721f8b64703bd6
juanchomarengo <juanmmarengo@gmail.com>
2022-09-13 09:19:46 -0300

    aesthetic fixes and action of the delete hook modal

--- src/pages/webhook/[id].js
+++ src/pages/webhook/[id].js
@@ -1,11 +1,13 @@
-import React, {useEffect, useState} from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useLocalStorage } from "@mantine/hooks";
 import { Edit, Trash } from "tabler-icons-react";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useQuery } from "@tanstack/react-query";
 import {
-  Center, Container,
+  Center,
+  Container,
   Divider,
   Grid,
   Loader,
@@ -13,7 +15,7 @@ import {
   Stack,
   Text,
 } from "@mantine/core";
-import { useModals } from '@mantine/modals';
+import { useModals } from "@mantine/modals";
 
 import { deleteHook, getHook, getHookHits } from "../../services/api";
 import EditCronhook from "../../components/actions/EditCronhook";
@@ -27,33 +29,38 @@ const WebhookId = () => {
 
   const { id } = router.query;
 
-  const { isLoading: loadingHook, data: hook, refetch: refetchHook } = useQuery(["cronhook"], () =>
-    getHook(accessToken, id)
-  );
+  const {
+    isLoading: loadingHook,
+    data: hook,
+    refetch: refetchHook,
+  } = useQuery(["cronhook"], () => getHook(accessToken, id));
 
-  const { isLoading: loadingHits, data: hookHits, refetch: refetchHits } = useQuery(
-    ["hits"],
-    () => getHookHits(accessToken, id)
-  );
+  const {
+    isLoading: loadingHits,
+    data: hookHits,
+    refetch: refetchHits,
+  } = useQuery(["hits"], () => getHookHits(accessToken, id));
 
   useEffect(() => {
-    refetchHook()
-    refetchHits()
-  }, [id])
+    refetchHook();
+    refetchHits();
+  }, [id]);
 
-
-  const openModalDelete = () =>
+  const openDeleteModal = () =>
     modals.openConfirmModal({
-      title: "Please confirm your action",
+      title: "Delete this hook",
+      centered: true,
       children: (
-        <Text size="sm">q
-          This action is so important that you are required to confirm it with a
-          modal. Please click one of these buttons to proceed.
+        <Text size="sm">
+          Are you sure you want to delete this hook? This action is irreversible
         </Text>
       ),
-      labels: { confirm: "Confirm", cancel: "Cancel" },
-      onCancel: () => console.log("Cancel"),
-      onConfirm: () => console.log("Confirmed"),
+      labels: { confirm: "Delete hook", cancel: "No don't delete it" },
+      confirmProps: { color: "red" },
+      onConfirm: () => {
+        deleteHook(accessToken, id);
+        router.push("/");
+      },
     });
 
   if (loadingHook || loadingHits) {
@@ -67,46 +74,46 @@ const WebhookId = () => {
   if (hook && hookHits) {
     return (
       <Container size={"md"}>
-          <Paper shadow="xs" p="md">
-            <Grid>
-              <Grid.Col span={7}>
-                <Link href="/">{`<- Back`}</Link>
-              </Grid.Col>
-              <Grid.Col span={1} offset={3}>
-                <Edit
-                  onClick={() => setOpenedEditModal(true)}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                />
-              </Grid.Col>
-              <Grid.Col span={1}>
-                <Trash
-                  onClick={openModalDelete}
-                  style={{
-                    cursor: "pointer",
-                  }}
-                />
-              </Grid.Col>
-            </Grid>
-            <Divider my="sm" variant="dotted" />
-            <Stack align="flex-start" spacing="xs">
-              <p>Hook: {hook.id}</p>
-              <p>URL: {hook.url}</p>
-              <p>Method: {hook.method}</p>
-              <p>Cron: {hook.cron}</p>
-              <p>Hits: </p>
-              {hookHits.length > 0 ? (
-                hookHits.map((hit, i) => (
-                  <p key={`p${i}`}>
-                    {hit.started_at} {hit.response_status} - {hit.response}
-                  </p>
-                ))
-              ) : (
-                <p>No hits yet</p>
-              )}
-            </Stack>
-          </Paper>
+        <Paper shadow="xs" p="md">
+          <Grid>
+            <Grid.Col span={7}>
+              <Link href="/">{`<- Back`}</Link>
+            </Grid.Col>
+            <Grid.Col span={1} offset={3}>
+              <Edit
+                onClick={() => setOpenedEditModal(true)}
+                style={{
+                  cursor: "pointer",
+                }}
+              />
+            </Grid.Col>
+            <Grid.Col span={1}>
+              <Trash
+                onClick={openDeleteModal}
+                style={{
+                  cursor: "pointer",
+                }}
+              />
+            </Grid.Col>
+          </Grid>
+          <Divider my="sm" variant="dotted" />
+          <Stack align="flex-start" spacing="xs">
+            <p>Hook: {hook.id}</p>
+            <p>URL: {hook.url}</p>
+            <p>Method: {hook.method}</p>
+            <p>Cron: {hook.cron}</p>
+            <p>Hits: </p>
+            {hookHits.length > 0 ? (
+              hookHits.map((hit, i) => (
+                <p key={`p${i}`}>
+                  {hit.started_at} {hit.response_status} - {hit.response}
+                </p>
+              ))
+            ) : (
+              <p>No hits yet</p>
+            )}
+          </Stack>
+        </Paper>
         <EditCronhook
           setOpened={setOpenedEditModal}
           opened={openedEditModal}
